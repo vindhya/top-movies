31db43b98e8e3a3b621b4d3bfe80463fd54814c6
Vindhya Raviraj <vindhya.raviraj@gmail.com>
2019-04-10 16:38:29 -0400

    passing filtered list of popular movies to componentDidMount and  support for future years

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -5,6 +5,7 @@ class MovieList extends Component {
 	state = {
 			movies: [],
 			year: new Date().getFullYear(),
+			// year: 2018,
 			minPopularity: 10
 	};
 
@@ -32,18 +33,36 @@ class MovieList extends Component {
 	}
 
 	getMovies = async () => {
+		let pages = [];
+
 		// if the year is 2019, already know it will take 8 calls to the API to get all the data back
 		// this is done for page load speed so that the values of each page's promise do not have to be resolved
 		// to check the popularity >= 10 before moving on to the next page's promise
 		if (this.state.year === 2019) {
-			const pages = await Promise.all(this.getPromises(8));
-			return pages.map(page => page.data.results).flat();
+			const response = await Promise.all(this.getPromises(8));
+			pages = response.map(page => page.data.results);
 
 		// if the year is not 2019 (i.e., 2020+), we will have to resolve each page's promise to check that the
 		// popularity is still above 10. presentation of movies will be slower than if the year is 2019
 		} else {
-			return [];
+			let pageNum = 1;
+			let isPopular = true;
+
+			while (isPopular) {
+				const page = await this.callMovieDb(pageNum);
+				const lowPopularity = page.data.results.filter(movie => movie.popularity < this.state.minPopularity);
+				pages.push(page.data.results);
+
+				// check for any movies with too low of a popularity. if they exist, break the while loop
+				if (lowPopularity.length > 0) {
+					isPopular = false;
+				}
+
+				pageNum++;
+			}
 		}
+
+		return pages.flat().filter(movie => movie.popularity >= this.state.minPopularity);
 	}
 
 	componentDidMount = async () => {
