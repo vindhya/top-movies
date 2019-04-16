950feb6a47d5f1d422035662491fdc197a276d9e
Vindhya Raviraj <vindhya.raviraj@gmail.com>
2019-04-15 21:27:42 -0400

    refactored and added cast api requests

--- src/api/movieDb.js
+++ src/api/movieDb.js
@@ -4,7 +4,15 @@ const instance = axios.create({
 	baseURL: 'https://api.themoviedb.org/3'
 });
 
-export const requestMovies = (year, pageNum) => {
+const instanceBasicParam = apiPath => {
+	return instance.get(apiPath, {
+		params: {
+			api_key: process.env.REACT_APP_MOVIEDB_API_KEY
+		}
+	});
+};
+
+export const requestMovies = (currentYear, pageNum) => {
 	const apiPath = '/discover/movie';
 
 	return instance.get(apiPath, {
@@ -12,19 +20,25 @@ export const requestMovies = (year, pageNum) => {
 			api_key: process.env.REACT_APP_MOVIEDB_API_KEY,
 			page: pageNum,
 			sort_by: 'popularity.desc',
-			primary_release_year: year
+			primary_release_year: currentYear
 		}
 	});
 }
 
-export const requestMovieDetail = id => {
-	const apiPath = `/movie/${id}`;
+export const requestMovieDetail = movieId => {
+	return instanceBasicParam(`/movie/${movieId}`);
+}
 
-	return instance.get(apiPath, {
-		params: {
-			api_key: process.env.REACT_APP_MOVIEDB_API_KEY
-		}
-	});
+export const requestCastList = movieId => {
+	return instanceBasicParam(`/movie/${movieId}/credits`);
+}
+
+export const requestPersonDetails = personId => {
+	return instanceBasicParam(`/person/${personId}`);
+}
+
+export const requestPersonMovieCredits = personId => {
+	return instanceBasicParam(`/person/${personId}/movie_credits`);
 }
 
 export default instance;
